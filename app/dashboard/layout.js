2cf8a02d0900066fe9f229e899acaf3cfacf3d91
stanislavshvets <stanislavshvets.com@gmail.com>
2024-02-15 17:38:45 +0200

    commented session

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,23 +3,24 @@ import { options } from "@/app/api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import css from '@/styles/DashboardPages/dashboard_layout.module.css'
+import { next_logo_mini } from '@/components/Sidebar/assets/svg'
 
 
 const DashboardLayout = async ({ children }) => {
 
-    const session = await getServerSession(options);
-
-    if(!session) {
-        redirect('/api/auth/signin?callbackUrl=/dashboard')
-    }
-
-    const { user : {name, email, image} } = session;
-
-    console.log('session header---->' , session);
+    // const session = await getServerSession(options);
+    //
+    // if(!session) {
+    //     redirect('/api/auth/signin?callbackUrl=/dashboard')
+    // }
+    //
+    // const { user : {name, email, image} } = session;
+    //
+    // console.log('session header---->' , session);
 
     return (
         <main className={css.main}>
-            <Sidebar name={name} mail={email} img={image}/>
+            <Sidebar name={"user"} mail={'email'} img={next_logo_mini}/>
            { children }
         </main>
     );
