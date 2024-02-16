5ca090952564eb91ab4c4192bad90afb49e4800d
stanislavshvets <stanislavshvets.com@gmail.com>
2024-02-16 18:55:29 +0200

    add formuser

--- app/dashboard/analytics/page.js
+++ app/dashboard/analytics/page.js
@@ -1,11 +1,34 @@
-import css from "@/styles/DashboardPages/analytics/analytics.module.css";
+"use client"
 
-const Analytics = () => {
+import css from "@/styles/DashboardPages/users/users.module.css";
+import {useForm} from "react-hook-form";
+
+const Users = () => {
+
+
+        const { register, handleSubmit, watch, formState: { errors },} = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
     return (
         <div className={css.main_div}>
-            <h1>Analytics</h1>
+            <h1>Users</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label> Name
+                    <input  {...register("name")} />
+                </label>
+                <label> Phone
+                    <input {...register("phone", { required: true })} />
+                </label>
+                <label> Email
+                    <input {...register("email", { required: true })} />
+                </label>
+                {errors.exampleRequired && <span>This field is required</span>}
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
 
-export default Analytics;
+export default Users;
