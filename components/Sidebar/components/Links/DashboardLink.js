90d56c39b8e93549bd77a1b6727dcdb012fecd9c
stanislavshvets <stanislavshvets.com@gmail.com>
2024-03-01 12:20:26 +0200

    Fixed links

--- components/Sidebar/components/Links/DashboardLink.js
+++ components/Sidebar/components/Links/DashboardLink.js
@@ -5,17 +5,11 @@ import css from "@/styles/sidebar/link.module.css";
 
 const DashboardLink = (props) => {
 
-    const {name, link, icon, setModalClose, ModalClose} = props
-
-    const pathname = usePathname()
-
-    console.log("pathname---->", pathname);
-    console.log("link---->", link);
+    const {name, link, icon, setModalClose, ModalClose, active} = props
 
     return (
         <li className={css.li}>
-                                                     {/*Через то шо відбувається ререндер не працює pathname === {link}*/}
-            <Link href={link} title={name} className={`${ModalClose ? css.tooltip : ''} ${pathname === {link} ? `${css.active}` : ``}`} onClick={()=> setModalClose(true)}>
+            <Link href={link} title={name} className={`${ModalClose ? css.tooltip : ''} ${active ? `${css.active}` : ``}`} onClick={()=> setModalClose(true)}>
                 <Image src={icon} alt={`${name} icon`} width={29} height={29} priority/>
                 <span className={ModalClose ? css.hide : css.link}>{name}</span>
                 <span className={css.tooltip_content}>{name}</span>
--- components/Sidebar/components/Links/LinkList.js
+++ components/Sidebar/components/Links/LinkList.js
@@ -1,18 +1,23 @@
 import DashboardLink from "@/components/Sidebar/components/Links/DashboardLink";
 import css from "@/styles/sidebar/linklist.module.css";
+import {usePathname} from "next/navigation";
 
 const LinkList = (props) => {
 
     const { setModalClose, ModalClose, pages } = props
 
+    const pathname = usePathname()
+
     return (
         <div className={css.sidebar_links}>
             <ul className={css.ul}>
                 {pages.map((page) =>
-                    <DashboardLink key={page.id} name={page.name} link={page.link} icon={page.icon} setModalClose={setModalClose} ModalClose={ModalClose}/>)}
+                    <DashboardLink key={page.id} name={page.name} link={page.link} icon={page.icon} setModalClose={setModalClose} ModalClose={ModalClose} active={(pathname === page.link)}/>)}
             </ul>
         </div>
     );
 };
 
 export default LinkList;
+
+// active={(pathname === page.link) ? true : false}
